973eed4fa09be8133c6a35aa4b561c2783d90a64
gebymomole <gebymomole@gmail.com>
2021-04-22 23:52:03 +0700

    fungsi login

--- app/src/main/java/com/ggv/cryptocurrencystore/auth/LoginActivity.java
+++ app/src/main/java/com/ggv/cryptocurrencystore/auth/LoginActivity.java
@@ -3,12 +3,35 @@ import com.ggv.cryptocurrencystore.R;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import com.ggv.cryptocurrencystore.R;
-public class LoginActivity extends AppCompatActivity {
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.ggv.cryptocurrencystore.R;
+import com.ggv.cryptocurrencystore.services.AuthService;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText editTextUsername, editTextPassword;
+    Button btnmasuk;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextPassword =findViewById(R.id.editTextPassword);
+        btnmasuk = findViewById(R.id.btnMasuk);
+
+        AuthService authService = new AuthService(this);
+
+        btnmasuk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                authService.login(editTextUsername.getText().toString(), editTextPassword.getText().toString());
+
+            }
+        });
+
     }
+
+
 }
--- app/src/main/java/com/ggv/cryptocurrencystore/services/AuthService.java
+++ app/src/main/java/com/ggv/cryptocurrencystore/services/AuthService.java
@@ -1,4 +1,58 @@
 package com.ggv.cryptocurrencystore.services;
 
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class AuthService {
+
+    Context context;
+
+    public AuthService(Context context){
+        this.context = context;
+    }
+
+    public void login(String username, String password){
+        String url ="http://54.151.229.213:8000/auth/login";
+
+        RequestQueue queue = Volley.newRequestQueue(context);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+        (Request.Method.POST, url, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+               // textView.setText("Response: " + response.toString());
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // TODO: Handle error
+
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams(){
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("username", username);
+                params.put("password", password);
+                return params;
+            }
+        };
+
+        queue.add(jsonObjectRequest);
+
+    }
+
 }
