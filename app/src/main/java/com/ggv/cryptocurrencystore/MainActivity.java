c2ee78e8df514fed0b0c4beacfcd9de58597237a
vaniazamili <vaniazamili26@gmail.com>
2021-04-23 18:00:33 +0700

    conneting

--- app/src/main/java/com/ggv/cryptocurrencystore/MainActivity.java
+++ app/src/main/java/com/ggv/cryptocurrencystore/MainActivity.java
@@ -2,13 +2,21 @@ package com.ggv.cryptocurrencystore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.ggv.cryptocurrencystore.auth.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        intent = new Intent(MainActivity.this, LoginActivity.class);
+        finish();
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/ggv/cryptocurrencystore/auth/RegisterActivity.java
+++ app/src/main/java/com/ggv/cryptocurrencystore/auth/RegisterActivity.java
@@ -2,15 +2,57 @@ package com.ggv.cryptocurrencystore.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.ggv.cryptocurrencystore.R;
 
 public class RegisterActivity extends AppCompatActivity {
+    Button button;
+    EditText editTxtName, editTxtUser, editTxtEmail, editTxtPas;
+    Intent intent;
+
+    int success;
+
+    //private String url = Server.URL + "RegisterActivity.java";
+
+    private static final String TAG = RegisterActivity.class.getSimpleName();
+
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
+
+    String tag_json_obj = "json_obj_req";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        button = findViewById(R.id.button);
+        editTxtName = findViewById(R.id.editTxtName);
+        editTxtUser = findViewById(R.id.editTxtUser);
+        editTxtEmail = findViewById(R.id.editTxtEmail);
+        editTxtPas = findViewById(R.id.editTextPassword);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                finish();
+                startActivity(intent);
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
