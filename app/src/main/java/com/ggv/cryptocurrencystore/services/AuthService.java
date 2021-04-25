91787e85526d112fab0b8ef40884aa2ea111cf78
Gilang Windu Asmara <gilangwinduasmara2@gmail.com>
2021-04-25 22:50:15 +0700

    profile

--- app/src/main/java/com/ggv/cryptocurrencystore/services/AuthService.java
+++ app/src/main/java/com/ggv/cryptocurrencystore/services/AuthService.java
@@ -1,6 +1,8 @@
 package com.ggv.cryptocurrencystore.services;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -17,7 +19,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class AuthService {
-
+    String token;
     public interface ResultListener {
         void onSuccess(JSONObject response);
         void onFail(JSONObject response);
@@ -28,6 +30,55 @@ public class AuthService {
 
     public AuthService(Context context){
         this.context = context;
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
+        this.token = sharedPref.getString("token", "");
+    }
+
+    public void getUser(AuthService.ResultListener listener){
+        String url ="http://54.151.229.213:8000/auth/user";
+
+        RequestQueue queue = Volley.newRequestQueue(context);
+        JSONObject body = new JSONObject();
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, body, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            boolean success = response.getBoolean("success");
+                            if(success){
+                                listener.onSuccess(response);
+                            }else{
+                                listener.onFail(response);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        listener.onError(error);
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams(){
+                Map<String, String> params = new HashMap<String, String>();
+                return params;
+            }
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json; charset=utf-8");
+                headers.put("Authorization", "Bearer "+token);
+                return headers;
+            }
+        };
+
+        queue.add(jsonObjectRequest);
+
     }
 
     public void login(String username, String password, ResultListener listener){
@@ -85,8 +136,6 @@ public class AuthService {
         queue.add(jsonObjectRequest);
 
     }
-
-
 
     public void register(String name, String username, String email, String password,  ResultListener listener){
         String url ="http://54.151.229.213:8000/auth/register";
