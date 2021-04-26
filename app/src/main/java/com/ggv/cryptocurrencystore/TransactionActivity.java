5c4cdef1590aecc7b6f226bc4ea21d3ac111c105
Gilang Windu Asmara <gilangwinduasmara2@gmail.com>
2021-04-26 17:54:33 +0700

    buy and sell

--- app/src/main/java/com/ggv/cryptocurrencystore/TransactionActivity.java
+++ app/src/main/java/com/ggv/cryptocurrencystore/TransactionActivity.java
@@ -3,25 +3,99 @@ package com.ggv.cryptocurrencystore;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.VolleyError;
 import com.ggv.cryptocurrencystore.models.Asset;
+import com.ggv.cryptocurrencystore.services.AssetService;
+import com.ggv.cryptocurrencystore.services.TransactionService;
+import com.google.android.material.button.MaterialButton;
+
+import org.json.JSONObject;
 
 public class TransactionActivity extends AppCompatActivity {
     TextView textViewPrice;
     TextView textViewAssetName;
+    TextView textViewTotal;
     EditText editTextAmount;
+    MaterialButton materialButtonBuy, materialButtonSell;
+    Asset asset;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction);
-        Asset asset = getIntent().getParcelableExtra("asset");
+        asset = getIntent().getParcelableExtra("asset");
         textViewPrice = findViewById(R.id.textViewPrice);
         textViewAssetName = findViewById(R.id.textViewAssetName);
+        textViewTotal = findViewById(R.id.textViewTotal);
         editTextAmount = findViewById(R.id.editTextAmount);
+
+        materialButtonBuy = findViewById(R.id.materialButtonBuy);
+        materialButtonSell = findViewById(R.id.materialButtonSell);
 
         textViewPrice.setText(asset.getPrice_usd()+"");
         textViewAssetName.setText(asset.getName());
+
+        materialButtonBuy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createTransaction("BUY");
+            }
+        });
+
+        materialButtonSell.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createTransaction("SELL");
+            }
+        });
+
+        editTextAmount.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(editable.toString().isEmpty()){
+                    textViewTotal.setText(0+" usd");
+                    return;
+                }
+                double total = Double.valueOf(editable.toString()) * Double.valueOf(asset.getPrice_usd());
+                textViewTotal.setText(total+" usd");
+            }
+        });
     }
+
+    public void createTransaction(String status){
+        TransactionService transactionService = new TransactionService(TransactionActivity.this);
+        transactionService.create(asset.getAsset_id(), editTextAmount.getText().toString(), status, new AssetService.ResultListener() {
+            @Override
+            public void onSuccess(JSONObject response) {
+                Log.d("response", response.toString());
+            }
+
+            @Override
+            public void onFail(JSONObject response) {
+                Log.d("response", response.toString());
+            }
+
+            @Override
+            public void onError(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/ggv/cryptocurrencystore/services/TransactionService.java
+++ app/src/main/java/com/ggv/cryptocurrencystore/services/TransactionService.java
@@ -81,4 +81,58 @@ public class TransactionService {
         queue.add(jsonObjectRequest);
 
     }
+
+    public void create(String asset_id, String ammount, String status, AssetService.ResultListener listener){
+        String url ="http://54.151.229.213:8000/transactions";
+
+        RequestQueue queue = Volley.newRequestQueue(context);
+        JSONObject body = new JSONObject();
+        try {
+            body.put("asset_id", asset_id);
+            body.put("ammount", ammount);
+            body.put("status", status);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.POST, url, body, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            boolean success = response.getBoolean("success");
+                            if(success){
+                                listener.onSuccess(response);
+                            }else{
+                                listener.onFail(response);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        listener.onError(error);
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams(){
+                Map<String, String> params = new HashMap<String, String>();
+                return params;
+            }
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json; charset=utf-8");
+                headers.put("Authorization", "Bearer "+token);
+                return headers;
+            }
+        };
+
+        queue.add(jsonObjectRequest);
+
+    }
 }
